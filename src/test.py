b9451c5f031855882e6bdd16134fd0c10c858121
Tim Hanewich <tahanewich@live.com>
2024-06-24 18:46:55 -0400

    Use default trail length

--- src/test.py
+++ src/test.py
@@ -3,7 +3,6 @@ import rainbow
 import time
 
 s = rainbow.Strand(12, rainbow.MODE_RUN_TRAIL)
-s.trail_length = 7
 pixs = neopixel.Neopixel(12, 0, 22, "GRB")
 re = rainbow.RainbowEngine()
 re.add((s, pixs))
