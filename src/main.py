1489962d1dbac991848c5683705bee493fad3f1d
Tim Hanewich <tahanewich@live.com>
2024-06-24 19:25:53 -0400

    Updated test

--- src/main.py
+++ src/main.py
@@ -8,13 +8,13 @@ def test() -> None:
     re = RainbowEngine()
 
     # add strand 1
-    s1 = Strand(5, MODE_TWINKLE)
-    s1.new_twinkle_chance = 0.05
+    s1 = Strand(5, MODE_TRAIL)
+    s1.trail_length = 3
     re.add((s1, neopixel.Neopixel(5, 0, 22, "GRB")))
 
     # add strand 2
-    s2 = Strand(12, MODE_TWINKLE)
-    s2.new_twinkle_chance = 0.1
+    s2 = Strand(12, MODE_TRAIL)
+    s2.trail_length = 5
     re.add((s2, neopixel.Neopixel(12, 1, 19, "GRB"))) # has to use a different StateMachine than the first. 0-7. Can only do 8 unique strands.
 
     while True:
